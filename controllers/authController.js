c300fc6a9db1496c65b1905f47ec12e7330a5b26
Dan K <KC135Q@users.noreply.github.com>
2021-05-30 15:29:45 -0400

    Basic auth route in place

---
+++ controllers/authController.js
@@ -0,0 +1,16 @@
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+const jwtDecode = require("jwt-decode");
+const db = require("../db/models/");
+
+const authPerson = async (req, res) => {
+  try {
+    return res.status(200).json({ message: "authPerson 200" });
+  } catch (error) {
+    return res.status(error.msg).json(error);
+  }
+};
+
+module.exports = {
+  authPerson,
+};
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,12 @@
 var express = require("express");
 var router = express.Router();
+const { authPerson } = require("../controllers/authController");
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
   res.render("index", { title: "Express" });
 });
 
-router.post("/");
+router.post("/auth", authPerson);
 
 module.exports = router;
