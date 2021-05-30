73b4ce7f062cdc5c0fad29b8ac6be2f4114caa6f
Dan K <KC135Q@users.noreply.github.com>
2021-05-30 15:32:30 -0400

    req body checks on auth

--- controllers/authController.js
+++ controllers/authController.js
@@ -5,6 +5,7 @@ const db = require("../db/models/");
 
 const authPerson = async (req, res) => {
   try {
+    console.log(req.body);
     return res.status(200).json({ message: "authPerson 200" });
   } catch (error) {
     return res.status(error.msg).json(error);
