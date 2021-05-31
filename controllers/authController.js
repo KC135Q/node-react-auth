3bbe7671e643f78bcdece29688fce4390c20c0de
Dan K <KC135Q@users.noreply.github.com>
2021-05-31 12:45:50 -0400

    Auth POST works

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,17 +1,17 @@
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
 const jwtDecode = require("jwt-decode");
-const db = require("../db/models");
+const { models } = require("../db/models");
 
 const authPerson = async (req, res) => {
   try {
     // console.log(req.body);
     const { email, password } = req.body;
-    console.log("db", db);
-    // const person = await User.find({ email: email }).exec();
-    // console.log(person);
+    // console.log("db", db);
+    const person = await models.User.find({ email }).exec();
+    console.log(person);
 
-    return res.status(200).json({ message: "authPerson 200" });
+    return res.status(200).json(person);
   } catch (error) {
     console.warn(error);
     return res.json(error);
--- scratch.js
+++
@@ -1,6 +0,0 @@
-// Hashes password automatically
-userSchema.pre("save", async function (next) {
-  const hash = await bcrypt.hash(this.password, Number(bcryptSalt));
-  this.password = hash;
-  next();
-});
